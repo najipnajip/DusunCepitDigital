e0b7a53d48a44922c9b65012b1d92a5c6de42997
Najip <nazhifsaja@gmail.com>
2026-05-12 23:41:56 +0700

    chore: cleanup astro config and remove unused node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
-
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
